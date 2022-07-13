eec264f63876ab7d32923beae5be28f3e6660c54
chris <chrisabbod@gmail.com>
2022-07-13 16:41:32 -0400

    Add timer functionality to custom progress bar view

--- app/src/main/java/com/chrisabbod/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/chrisabbod/a7minuteworkout/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package com.chrisabbod.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.chrisabbod.a7minuteworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +28,50 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        setUpRestView()
+
+    }
+
+    private fun setUpRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressbar?.progress = restProgress
+
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressbar?.progress = 10 - restProgress
+
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }.start()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        binding = null
     }
 }

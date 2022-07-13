8d5a10fa71f85d0fa784b609b6ac39fac1339370
chris <chrisabbod@gmail.com>
2022-07-13 00:26:01 -0400

    Creat start UI with start button

--- app/src/main/java/com/chrisabbod/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.chrisabbod.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {
+            Toast.makeText(this@MainActivity,
+            "Here we will start the main application",
+            Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }

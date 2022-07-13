53f9e38f24b7718cf57de52cce63cd4c77f6e591
chris <chrisabbod@gmail.com>
2022-07-13 14:24:03 -0400

    Add view binding to project

--- app/src/main/java/com/chrisabbod/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/a7minuteworkout/MainActivity.kt
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.chrisabbod.a7minuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(this@MainActivity,
-            "Here we will start the main application",
-            Toast.LENGTH_SHORT
+                "Here we will start the main application",
+                Toast.LENGTH_SHORT
             ).show()
         }
     }

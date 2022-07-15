29922bfbc3bbf6a59c83569521763ca504eb3ee5
chris <chrisabbod@gmail.com>
2022-07-15 11:09:26 -0400

    Current exercise recyclerview item is white with green stroke, after completion turns completely green

--- app/src/main/java/com/chrisabbod/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/chrisabbod/a7minuteworkout/ExerciseActivity.kt
@@ -130,6 +130,10 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
             override fun onFinish() {
                 currentExercisePosition++
+
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
                 setUpExerciseView()
             }
 
@@ -149,6 +153,11 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
 
             override fun onFinish() {
+
+                exerciseList!![currentExercisePosition].setIsSelected(false)
+                exerciseList!![currentExercisePosition].setIsCompleted(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
                 if (currentExercisePosition < exerciseList?.size!! - 1) {
                     setUpRestView()
                 } else {
--- app/src/main/java/com/chrisabbod/a7minuteworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/chrisabbod/a7minuteworkout/ExerciseStatusAdapter.kt
@@ -1,17 +1,19 @@
 package com.chrisabbod.a7minuteworkout
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.chrisabbod.a7minuteworkout.databinding.ItemExerciseStatusBinding
 
-class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>):
+class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
     RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
 
-        inner class ViewHolder(binding: ItemExerciseStatusBinding):
-            RecyclerView.ViewHolder(binding.root) {
-            val tvItem = binding.tvItem
-        }
+    inner class ViewHolder(binding: ItemExerciseStatusBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val tvItem = binding.tvItem
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(ItemExerciseStatusBinding.inflate(LayoutInflater.from(parent.context)))
@@ -20,6 +22,33 @@ class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>):
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val model: ExerciseModel = items[position]
         holder.tvItem.text = model.getId().toString()
+
+        when {
+            model.getIsSelected() -> {
+                holder.tvItem.background =
+                    ContextCompat.getDrawable(
+                        holder.itemView.context,
+                        R.drawable.item_circular_thin_color_accent_border
+                    )
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+            model.getIsCompleted() -> {
+                holder.tvItem.background =
+                    ContextCompat.getDrawable(
+                        holder.itemView.context,
+                        R.drawable.item_circular_color_accent_background
+                    )
+                holder.tvItem.setTextColor(Color.parseColor("#FFFFFF"))
+            }
+            else -> {
+                holder.tvItem.background =
+                    ContextCompat.getDrawable(
+                        holder.itemView.context,
+                        R.drawable.item_circular_color_gray_background
+                    )
+                holder.tvItem.setTextColor(Color.parseColor("#212121"))
+            }
+        }
     }
 
     override fun getItemCount(): Int {

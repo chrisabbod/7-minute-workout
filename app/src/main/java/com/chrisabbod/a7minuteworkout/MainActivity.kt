e3c45da86a2cee8c1b11d0a968698a478e128413
chris <chrisabbod@gmail.com>
2022-07-16 16:41:37 -0400

    Add history button and intent to start activity

--- app/src/main/java/com/chrisabbod/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/a7minuteworkout/MainActivity.kt
@@ -23,5 +23,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, BmiActivity::class.java)
             startActivity(intent)
         }
+
+        binding?.flHistory?.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
